31e32860a8cb15def03a1609bbf5c8f07ae4362e
Sylvie Aucoin <syaucoi@LLC02CTHWXMD6M.ngco.com>
2021-03-16 10:14:11 -0400

    Blog app completed

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,23 +1,48 @@
+import _ from 'lodash'; 
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    // console.log('about to fetch posts!');
+    await dispatch(fetchPosts());
+    // console.log('fetched posts!');
+    // console.log(getState().posts);
+
+    //Use lodash library to map over posts and get unique user ids
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    //call fetchUSer
+    userIds.forEach(id => dispatch(fetchUser(id)));
+};
 
 
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
     
-    dispatch({
-        type: 'FETCH_POSTS',
-        payload: response.data
-    })
+    dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
 export const fetchUser = (id) => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
 
-    dispatch({
-        type: 'FETCH_USER',
-        payload: response.data
-    })
-}
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+};
+
+
+
+
+//MEMOIZED VERSION
+// //memoize to avoid calling everytime we want to get a user id. This way we only need to call it once.
+
+// export const fetchUser = (id) => dispatch => {
+//     _fetchUser(id, dispatch);
+// };
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//     dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
+
+
 
 //Bad approach!!!! We need to use redux-thunk!
 //1. because of the async/await, we are returning the request, which is not an object
--- blog/src/components/UserHeader.js
+++ blog/src/components/UserHeader.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchUser } from '../actions';
 
 class UserHeader extends Component {
-    componentDidMount(){
-        this.props.fetchUser(this.props.userId);
-    }
-    
     render(){
-        return(
-            <div>User Header</div>
-        );
-    };
-};
+    const { user } = this.props;     
 
-export default connect(null, { fetchUser })(UserHeader);
+        if (!user){
+            return null;
+        }
+
+        return(
+            <div>{user.name}</div>
+        );
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    return { user: state.users.find(user => user.id === ownProps.userId) };
+}
+
+export default connect(mapStateToProps)(UserHeader);
